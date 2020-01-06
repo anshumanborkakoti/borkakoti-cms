8bcc24af3d4ef986b552214500e939596c0c90e1
Anshuman Borkakoti <anshumanborkakoti@gmail.com>
2020-01-06 22:42:48 +0530

    Added staging configuration, changed to use TLJNE cloudinary

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // api_url: 'https://api.the-little-journal.com/tljneapi',
-  // cloudinary_cloud_name: 'dcom6pwyq',
-  // cloudinary_upload_preset: 'l3e7bqht',
-  // cloudinary_api_key: '434365393345825',
-  // cloudinary_folder_name: 'tljnesandbox'
+  cloudinary_cloud_name: 'the-little-journal-com',
+  cloudinary_folder_name: 'tljne',
+  cloudinary_upload_preset: 'unsigned_default',
+  cloudinary_api_key: '533629619274347',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,14 @@ export const environment = {
   production: false,
   // api_url: 'http://localhost:3000/tljneapi',
   api_url: 'http://tljne-env.kmhieh52tp.ap-south-1.elasticbeanstalk.com/tljneapi',
-  cloudinary_cloud_name: 'dcom6pwyq',
-  cloudinary_upload_preset: 'l3e7bqht',
-  cloudinary_api_key: '434365393345825',
-  cloudinary_folder_name: 'tljnesandbox'
+  cloudinary_cloud_name: 'the-little-journal-com',
+  cloudinary_folder_name: 'tljne',
+  cloudinary_upload_preset: 'unsigned_default',
+  cloudinary_api_key: '533629619274347',
+  // cloudinary_cloud_name: 'dcom6pwyq',
+  // cloudinary_upload_preset: 'l3e7bqht',
+  // cloudinary_api_key: '434365393345825',
+  // cloudinary_folder_name: 'tljnesandbox'
 };
 
 /*
