5611dcb7c4d3097bb6f8b9b7a391d708e5ed0ccf
Anshuman Borkakoti <anshumanborkakoti@gmail.com>
2020-01-22 23:32:26 +0530

    Added npmrc for bcrypt aws install, removed env files from github

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  // api_url: 'https://api.the-little-journal.com/tljneapi',
-  cloudinary_cloud_name: 'the-little-journal-com',
-  cloudinary_folder_name: 'tljne',
-  cloudinary_upload_preset: 'unsigned_default',
-  cloudinary_api_key: '533629619274347',
-};
