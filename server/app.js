e47d609c9289a1161bde0190cbc1874bff1383a4
Anshuman Borkakoti <anshumanborkakoti@gmail.com>
2020-01-25 10:39:57 +0530

    Moved localhost origins to non-prod

--- server/app.js
+++ server/app.js
@@ -33,18 +33,22 @@ mongoose
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
-const allowedOrgins = [
+const allowedOrigins = [
   'https://the-little-journal.com',
   'https://www.the-little-journal.com',
-  'http://localhost:4200',
-  'http://localhost:4201',
-  'http://tljne-staging.s3-website.ap-south-1.amazonaws.com',
   'https://cms.the-little-journal.com',
+  'http://tljne-staging.s3-website.ap-south-1.amazonaws.com',
   'http://tljne-cms.s3-website.ap-south-1.amazonaws.com'
 ];
+if (!utils.isProd()) {
+  allowedOrigins.push(
+    'http://localhost:4200',
+    'http://localhost:4201'
+  );
+}
 app.use((req, res, next) => {
   const reqOrigin = req.headers['origin'];
-  const originIndex = allowedOrgins.findIndex(aOrigin => aOrigin === reqOrigin);
+  const originIndex = allowedOrigins.findIndex(aOrigin => aOrigin === reqOrigin);
   if (originIndex > -1) {
     res.setHeader("Access-Control-Allow-Origin", reqOrigin);
     res.setHeader("Vary", 'Origin');
